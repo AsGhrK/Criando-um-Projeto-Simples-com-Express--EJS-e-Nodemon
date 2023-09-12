cce00056a5504bbf60d2e09e23523a680df7a765
AsGhrK <131817273+AsGhrK@users.noreply.github.com>
2023-09-12 14:30:01 -0400

    12/09

--- meu-projeto-express-ejs/App.js
+++ meu-projeto-express-ejs/App.js
@@ -5,9 +5,16 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+// controller ou rota
+app.get('/', (req, res) => { 
+  res.render('index', { message: 'Olá, Mundo!HAHAHAHAHAHAH' });
 });
+//
+
+app.get('/1', (req, res) => { 
+  res.render('contato', { message: 'Olá, Mundo!HAHAHAHAHAHAHahhahahahahahaha' });
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
