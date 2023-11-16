cad23667ab1d68a683f12c09b4418f3a4c990dae
AsGhrK <davicasari1@gmail.com>
2023-11-16 14:17:30 -0400

    js no html

--- meu-projeto-express-ejs/App.js
+++ meu-projeto-express-ejs/App.js
@@ -7,16 +7,24 @@ app.use(express.static('public'));
 
 // controller ou rota
 app.get('/', (req, res) => { 
-  res.render('index', { message: 'Olá, Mundo!HAHAHAHAHAHAH' });
+  res.render('index', {produtos});
 });
-//
+
 
 const produtos =[
   {id:1, nome: "nome",preco: 1000, dricricao:"discricao", imagem:""},
   {id:2, nome: "nome",preco: 1000, dricricao:"discricao", imagem:""},
-  {id:3, nome: "nome",preco: 1000, dricricao:"discricao", imagem:""}
+  {id:3, nome: "nome",preco: 1000, dricricao:"discricao", imagem:""},
+  {id:4, nome: "nome",preco: 1000, dricricao:"discricao", imagem:""},
+  {id:5, nome: "nome",preco: 1000, dricricao:"discricao", imagem:""}
 ]
 
+function buscarProdutosPoriD(id){
+  const produto = produtos.find(produto => produto.id == id)
+  return produto || null
+}
+
+console.log(buscarProdutosPoriD(2))
 
 app.get('/monitor.ejs', (req, res) => { 
   res.render('monitor');
@@ -29,6 +37,18 @@ app.get('/mouse.ejs', (req, res) => {
 
 app.get('/teclado.ejs', (req, res) => { 
   res.render('teclado');
+});
+
+app.get('/pc.ejs', (req, res) => { 
+  res.render('pc');
+});
+
+app.get('/suporte.ejs', (req, res) => { 
+  res.render('suporte');
+});
+
+app.get('/cadeira.ejs', (req, res) => { 
+  res.render('cadeira');
 });
 
 app.get('/index.ejs', (req, res) => { 
